306503c3f1f99ae73c13ac7734d705e5f216c1f4
Jason Zhang <jzhang@riverbed.com>
2014-04-24 07:16:45 -0700

    turn off flymake for python - too much distraction and easy to break.

--- jason-cscope.el
+++ jason-cscope.el
@@ -1,7 +1,7 @@
 ;; xcscope
-(setq cscope-do-not-update-database t)
 (require 'xcscope)
 (cscope-setup)
 (add-hook 'python-mode-hook (function cscope-minor-mode))
 (add-hook 'sh-mode-hook (function cscope-minor-mode))
+(setq cscope-do-not-update-database t)
 
--- jason-python.el
+++ jason-python.el
@@ -103,17 +103,17 @@
 
 ;; pyflakes flymake integration
 ;; http://stackoverflow.com/a/1257306/347942
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-(add-hook 'python-mode-hook 'flymake-mode)
+;; (when (load "flymake" t)
+;;   (defun flymake-pyflakes-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       (list "pyflakes" (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pyflakes-init)))
+;; (add-hook 'python-mode-hook 'flymake-mode)
 
 ;; todo
 ;; look intp integrating pep8 checker

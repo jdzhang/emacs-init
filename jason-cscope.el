53df3d2b5c4abbc89313680b2e7be96d944fd1e9
Jason Zhang <jdzhang@gmail.com>
2014-12-04 10:44:59 -0800

    added function to set kernel mode set use inverted index by default

--- jason-cscope.el
+++ jason-cscope.el
@@ -15,4 +15,16 @@
 (add-hook 'sh-mode-hook (function cscope-minor-mode))
 (add-hook 'nxml-mode-hook (function cscope-minor-mode))
 (setq cscope-do-not-update-database t)
+(setq cscope-option-use-inverted-index t)
 
+(defun cscope-set-kernel-mode ()
+  "switch cscope into kernel mode and use reverted index"
+  (interactive)
+  (setq cscope-option-kernel-mode t)
+  )
+
+(defun cscope-reset-kernel-mode ()
+  "switch to default"
+  (interactive)
+  (setq cscope-option-kernel-mode nil)
+  )

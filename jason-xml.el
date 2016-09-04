46cdb4bdab5006a80b3252f4f954bf036cbaacb7
Jason Zhang <jzhang@jason-lt.local>
2016-09-04 11:05:08 -0700

    add yaml

--- jason-xml.el
+++ jason-xml.el
@@ -1,4 +1,17 @@
-(setq auto-mode-alist
-      (cons '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode)
-            auto-mode-alist))
+;; xml & other language modes
+
+(defvar data-packages '(
+                        yaml-mode
+                        )
+  "A list of packages for data format modes")
+
+(dolist (p data-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+
+(add-to-list 'auto-mode-alist '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode))
 (setq nxml-child-indent 4)
+

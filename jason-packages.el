c5623beb0939b5ec2468e8683d5edad2555f572f
Jason Zhang <jzhang@jason-lt.local>
2016-09-04 11:03:26 -0700

    update melpha url, remove marmalade.

--- jason-packages.el
+++ jason-packages.el
@@ -2,8 +2,7 @@
 ;;
 
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (when (not package-archive-contents)

a371cdaefa3cedaa8fb736ebe25b33e978264c86
Jason Zhang <jzhang@jason-lt.local>
2016-09-04 19:56:46 -0700

    rename the files, added local/bin to path

--- init.el
+++ init.el
@@ -1,12 +1,19 @@
+;; copy this file to .emacs in homedir
+;;
+;;; Mac OS X Setup
+(when (eq system-type 'darwin)
+  (setenv "PATH" (concat "/usr/local/go/bin:/usr/local/bin:/Users/jzhang/bin:" (getenv "PATH")))
+  (setq exec-path (append '("/usr/local/go/bin" "/usr/local/bin") exec-path))
+  )
+
 (add-to-list 'load-path "~/.emacs.d/lisp")
-(load "jason-packages")
-(load "jason-general")
-(load "jason-versioncontrol")
-(load "jason-cmode")
-(load "jason-python")
-(load "jason-xml")
-(load "jason-cscope")
-;;(load "startup")
+(load "init-packages")
+(load "init-general")
+(load "init-versioncontrol")
+(load "init-cmode")
+(load "init-python")
+(load "init-xml")
+(load "init-cscope")
 
 ;; (setq x-select-enable-clipboard t)
 
